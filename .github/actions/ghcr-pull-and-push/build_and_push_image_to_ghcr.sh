72ea77b7d8fcc464a5dfd9b3aee304c52d425f15
nikinov <novellenicholas@gmail.com>
2025-04-16 14:21:35 +0200

    changed wazuh to nikinov

--- .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 GITHUB_REPOSITORY="wazuh/wazuh"
-GITHUB_OWNER="wazuh"
+GITHUB_OWNER="nikinov"
 IMAGE_ID_CACHE=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:latest
 IMAGE_ID_CACHE=$(echo ${IMAGE_ID_CACHE} | tr '[A-Z]' '[a-z]')
 IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
