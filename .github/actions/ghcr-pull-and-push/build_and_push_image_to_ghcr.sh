f738cc0e56d998a860896b36f9b58b5a3b20cd40
nikinov <novellenicholas@gmail.com>
2025-04-18 07:55:06 +0200

    renamed from guardbear2 to nikinov

--- .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
@@ -8,8 +8,8 @@ if [ -n "$5" ]; then
 else
     exit 1
 fi
-GITHUB_REPOSITORY="GuardBear2/SIEM"
-GITHUB_OWNER="guardbear2"
+GITHUB_REPOSITORY="nikinov/wazuh"
+GITHUB_OWNER="nikinov"
 IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
--- .github/actions/ghcr-pull-and-push/pull_image_from_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/pull_image_from_ghcr.sh
@@ -7,8 +7,8 @@ if [ -n "$4" ]; then
 else
     exit 1
 fi
-GITHUB_REPOSITORY="GuardBear2/SIEM"
-GITHUB_OWNER="wazuh"
+GITHUB_REPOSITORY="nikinov/wazuh"
+GITHUB_OWNER="nikinov"
 IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
